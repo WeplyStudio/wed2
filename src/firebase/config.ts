8faa1bc740cccbcabd7d8b482dd9cf484395d506
Jaysss <matchboxdevelopment@gmail.com>
2026-03-13 10:06:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1934680313-ffb4b",
+  "appId": "1:148215929399:web:b575515b071dbb4caa7db2",
+  "apiKey": "AIzaSyDTfLEYsSfF_SRV8WYULoCSC-UqcvzcBOs",
+  "authDomain": "studio-1934680313-ffb4b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "148215929399"
+};
